1d25cd5565d7c0c62ae62c81c34fa9aaa7d19682
Erik Turøy Midtun <etm.midtun@gmail.com>
2019-10-10 20:10:46 +0200

    setup the project and made the fetchXML parser

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # yr-xml2json
+
+api for fetching data from yr.no in xml format and converting it into a minimized json file.
+
+url input format: "https://www.yr.no/place/Norway/Tr%C3%B8ndelag/Trondheim/Trondheim/forecast.xml"
+
+json output format:
+´´´javascript
+{
+  "name": "Trondheim",
+  "fetched": "2019-9-10T19:48:50",
+  "forecast": [
+    {
+      "from": "2019-10-10T20:00:00",
+      "to": "2019-10-11T00:00:00",
+      "symbol": "01n",
+      "rain": { "value": "0" },
+      "temperature": "5"
+    },
+    {
+      "from": "2019-10-11T00:00:00",
+      "to": "2019-10-11T06:00:00",
+      "symbol": "40n",
+      "rain": { "value": "0.8", "minvalue": "0.5", "maxvalue": "1.2" },
+      "temperature": "3"
+    },
+    {
+      "from": "2019-10-11T06:00:00",
+      "to": "2019-10-11T12:00:00",
+      "symbol": "03d",
+      "rain": { "value": "0", "minvalue": "0", "maxvalue": "0.3" },
+      "temperature": "5"
+    },
+    {
+      "from": "2019-10-11T12:00:00",
+      "to": "2019-10-11T18:00:00",
+      "symbol": "04",
+      "rain": { "value": "0" },
+      "temperature": "8"
+    }
+  ]
+}
+´´´
+
+symbol is used to fetch png from url on format:
+https://yr.github.io/weather-symbols/png/100/< symbol >.png
---
+++ fetchXML.js
@@ -0,0 +1,66 @@
+const unirest = require('unirest');
+const convert = require('xml-js');
+const fs = require('fs');
+
+
+const fetchXML = (url , numberOfForecasts = 4) => {
+    unirest.get(url).end( result => {
+        const xml = result.body;
+        const js = convert.xml2js(xml, {compact: true, spaces: 1});
+        const n = numberOfForecasts - 1 //number of forecasts, 0-indexed
+        const finalObj = parse(js, n) 
+        const file =  JSON.stringify(parse(js, n));
+        writeToFile( file );
+        return finalObj;
+    });
+}
+
+// writes to forecast.json
+const writeToFile = ( filetext ) => {
+    fs.writeFile("forecast.json", filetext, (err) => {
+        if(err) {
+            return console.log(err);
+        }
+    })  
+}
+
+/* parse the js object into the specification in README */
+const parse = ( obj, n ) => {
+    const forecast = obj.weatherdata.forecast;
+    const location = forecast.text.location._attributes.name;
+    const time = forecast.tabular.time;
+    const json = {};
+    const weather = [];
+    json["name"] = location;;
+    json["fetched"] = getTime();
+    //making the object to the specs like in readme
+    for (i = 0; i <= Math.min(n, time.length); i++) {
+        let obj = {};
+        const t = time[i]
+        obj["from"] = t._attributes.from;
+        obj["to"] = t._attributes.to;
+        obj["symbol"] = t.symbol._attributes.var;
+        obj["rain"] = t.precipitation._attributes;
+        obj["temperature"] = t.temperature._attributes.value;
+        weather[i] = obj;
+    } 
+    json["forecast"] = weather;
+    return json;
+}
+
+
+/* returns current time on format YYYY-MM-DDTHH:MM:SS */
+const getTime = () => {
+    const d = new Date();
+    const s = d.getFullYear() + "-" +
+              d.getMonth()    + "-" + 
+              d.getDate()     + "T" +
+              d.getHours()    + ":" + 
+              d.getMinutes()  + ":" +
+              d.getSeconds();
+    return s;
+}
+
+
+url = "https://www.yr.no/place/Norway/Tr%C3%B8ndelag/Trondheim/Trondheim/forecast.xml";
+fetchXML(url); 
